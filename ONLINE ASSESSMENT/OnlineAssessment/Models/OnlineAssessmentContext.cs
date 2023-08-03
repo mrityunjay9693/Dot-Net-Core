e3be6ff272f8e7b97e84840edbbac550d6127b1e
mrityunjay9693 <mkaykumar123@gmail.com>
2023-08-03 17:59:21 +0530

    online assessment project updates

--- ONLINE ASSESSMENT/OnlineAssessment/Models/OnlineAssessmentContext.cs
+++ ONLINE ASSESSMENT/OnlineAssessment/Models/OnlineAssessmentContext.cs
@@ -2,14 +2,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace OnlineAssessment.Models
 {
-    public class OnlineAssessmentContext: DbContext
+    public class OnlineAssessmentContext : DbContext
     {
 
-        public OnlineAssessmentContext(DbContextOptions<OnlineAssessmentContext> options): base(options)
+        public OnlineAssessmentContext(DbContextOptions<OnlineAssessmentContext> options) : base(options)
         {
-            
+
         }
-        public DbSet<Question>? Questions{ get; set; }
+        public DbSet<Question>? Questions { get; set; }
 
     }
 }
--- ONLINE ASSESSMENT/OnlineAssessment/Program.cs
+++ ONLINE ASSESSMENT/OnlineAssessment/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineAssessment.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<>
+builder.Services.AddDbContext<OnlineAssessmentContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("OnlineAssessmentConnectionString"))
+
+);
 
 var app = builder.Build();
 
