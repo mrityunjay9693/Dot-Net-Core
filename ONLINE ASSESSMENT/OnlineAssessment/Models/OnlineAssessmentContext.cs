14ebc8a16ffdab8d8f1ed9ed8c47be57043d224b
mrityunjay9693 <mkaykumar123@gmail.com>
2023-08-03 11:56:34 +0530

    online assessment project updates

--- ONLINE ASSESSMENT/OnlineAssessment/Models/OnlineAssessmentContext.cs
+++ ONLINE ASSESSMENT/OnlineAssessment/Models/OnlineAssessmentContext.cs
@@ -5,5 +5,11 @@ namespace OnlineAssessment.Models
     public class OnlineAssessmentContext: DbContext
     {
 
+        public OnlineAssessmentContext(DbContextOptions<OnlineAssessmentContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Question>? Questions{ get; set; }
+
     }
 }
