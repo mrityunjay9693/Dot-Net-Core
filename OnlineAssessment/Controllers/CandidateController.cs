c4949bcc7bfab73b97f8133e11991a0cf423d4ea
mrityunjay9693 <mkaykumar123@gmail.com>
2023-08-10 00:11:59 +0530

    Controller is created in project

---
+++ OnlineAssessment/Controllers/CandidateController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineAssessment.Controllers
+{
+    public class CandidateController : Controller
+    {
+
+    }
+}
+ 
---
+++ OnlineAssessment/Controllers/QuestionController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineAssessment.Controllers
+{
+
+    public class QuestionController : Controller
+    {
+        public string Index()
+        { 
+            return "Welcome to Online Assessment";
+        }
+    }
+
+}
